7dc80e57a8253ba87c68931f32fca3111453c893
Guneetconvent2002 <itsguneetsingh@gmail.com>
2022-03-27 15:22:34 +0530

    assests and script added

---
+++ Main Menu/Assets/Movement.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    /*
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }*/
+
+    [SerializeField] private Transform Place;
+
+    private Vector2 iniposition;
+
+    
+    private float deltaX, deltaY;
+    private Touch touch;
+    private static bool locked;
+
+    private void start()
+    {
+        iniposition = transform.position;
+    }
+
+    private void update()
+    {
+        if (Input.touchCount > 0 && !locked)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchpos = Camera.main.ScreenToWorldPoint(touch.position);
+
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchpos))
+                    {
+                        deltaX = touchpos.x - transform.position.x;
+                        deltaY = touchpos.x - transform.position.y;
+                    }
+                    break;
+                case TouchPhase.Moved:
+                    if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchpos))
+                    {
+                        transform.position = new Vector2(touchpos.x - deltaX, touchpos.y - deltaY);
+                    }
+                    break;
+                case TouchPhase.Ended:
+                    if (Mathf.Abs(transform.position.x - Place.position.x) <= 0.5f && Mathf.Abs(transform.position.x - Place.position.x) <= 0.5f)
+                    {
+                        transform.position = new Vector2(Place.position.x, Place.position.y);
+                        locked = true;
+                    }
+                    else
+                    {
+                        transform.position = new Vector2(iniposition.x, iniposition.y);
+                    }
+                    break;
+            }
+        }
+    }
+}
